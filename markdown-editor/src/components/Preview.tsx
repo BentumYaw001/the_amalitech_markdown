8cf0454c82752c5e1a690fc417da609e47912547
BentumYaw001 <richmondbentum2020@gmail.com>
2024-05-30 01:04:38 -0700

    first commit

---
+++ markdown-editor/src/components/Preview.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import { marked } from 'marked';
+import dompurify from 'dompurify';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/default.css'; 
+import 'highlight.js/styles/tokyo-night-dark.css';
+
+
+interface PreviewProps {
+    markdown: string;
+}
+
+
+marked.setOptions({
+    highlight: function (code: string, lang: string): string {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+    },
+    langPrefix: 'hljs language-'
+});
+
+marked.use({
+    async: true,
+    pedantic: false,
+    gfm: true,
+  });
+
+export default function Preview({ markdown }: PreviewProps) {
+    const [parsed, setParsed] = useState<string>('');
+
+    useEffect(() => {
+        const parseMarkdown = async () => {
+            let result: string | Promise<string> = marked.parse(markdown);
+            
+            // Ensure result is a string
+            if (result instanceof Promise) {
+                result = await result;
+            }
+            
+            setParsed(dompurify.sanitize(result));
+        };
+
+        parseMarkdown();
+    }, [markdown]);
+
+    return (
+        <div>
+            <div className="w-full bg-gray-900 p-3 text-gray-400 uppercase tracking-wider">
+                Preview
+            </div>
+            <div className="p-6 prose prose-invert prose-a:text-blue-500" dangerouslySetInnerHTML={{ __html: parsed }} />
+        </div>
+    );
+}
